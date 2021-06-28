703bc5e8f63b95d38922cd3c9bf0a1456da72170
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-06-28 14:21:20 -0400

    adding ajax GET, server changes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,33 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    //listener set up to send the jokes to the dom
+    $('#addJokeButton').on('click', addJoke)
+
+}
+
+
+function addJoke() {
+    console.log('Add joke button has been clicked');
+
+    
+    //post gathering from input fields
+    //server will calculate
+
+  $.ajax({
+    method: 'POST',
+    url: '/jokes',
+    data:  {
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine: $('#punchlineIn').val(),
+    }
+  })
+  .then(result => {
+    console.log('POST /joke added', result);
+    getOperation();
+  })
+  .catch(err => {
+    console.log('POST /adding joke failed fail', err);
+  })
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require( 'express' );
 const app = express();
-const bodyParser = require( 'body-parser' );
+// const bodyParser = require( 'body-parser' );
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// serve back static files
+app.use(express.static('server/public'));
+
+
+app.listen(PORT, () => {
+  console.log('server running on: ', PORT);
+}); // end spin up server
 
 let jokes = [
   {
@@ -34,9 +42,5 @@ let jokes = [
   }
 ];
 
-// serve back static files
-app.use(express.static('server/public'));
 
-app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
-}); // end spin up server
+
